88a0d53cb5c60e006d952d5a20b05781444340e1
unknown <ijihad639@gmail.com>
2025-02-17 23:36:26 +0600

    3d

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
   toast[type](message);
 };
 
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://todo-mern-api-beta.vercel.app";
